6827db3a51c362a16eb0ebb3390c6049d58f3cf8
Dennis Yeung <otyeung2017@gmail.com>
2020-11-15 22:09:14 +0800

    add GET contacts method to Firebase functions

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,10 @@
+const firebase = require("firebase/app");
 const functions = require('firebase-functions')
 // const axios = require('axios')
 
 // #### Firebase database initialization
 // Firebase App (the core Firebase SDK) is always required and
 // must be listed before other Firebase SDKs
-const firebase = require("firebase/app");
 
 // Add the Firebase products that you want to use
 //require("firebase/auth");
@@ -22,7 +22,10 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase, returns an object
-const database = firebase.initializeApp(firebaseConfig)
+//const database = firebase.initializeApp(firebaseConfig)
+firebase.initializeApp(firebaseConfig)
+var database = firebase.database()
+var ref = database.ref('contacts')
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -46,14 +49,28 @@ app.get('/admin', (req, res) => {
     res.send('GET /api/admin is working')
 })
 
+// Return all contacts from db
+app.get('/contacts', (req, res) => {
+  const ref = database.ref('contacts')
+
+  // Attach an asynchronous callback to read the data at our posts reference
+  ref.on("value", function(snapshot) {
+    console.log(snapshot.val())
+    res.send(snapshot.val())
+  }, function (errorObject) {
+    console.log("The read failed: " + errorObject.code)
+    res.send("The read failed: " + errorObject.code)
+  })
+})
+
 // POST https://us-central1-react-crud-d487b.cloudfunctions.net/api/contacts route
 app.post('/contacts', (req, res) => {
-    const contact = {
+    var contact = {
         fullName : req.body.fullName,
         address : req.body.address,
         email : req.body.email,
         mobile : req.body.mobile
-    };    
+    }   
 
     // Method 1 : use HTTP post to add record into realtime database
 /*       axios.post('https://react-crud-d487b.firebaseio.com/contacts.json', contact)
@@ -68,8 +85,15 @@ app.post('/contacts', (req, res) => {
 
     // Method 2 : use Firebase SDK to add record into realtime database
     // Authentication is disabled to make it simple
-    const ref = database.ref('contacts')
     ref.push(contact)
-});
+      .then((response) => {
+        console.log("Added a new contact to Firebase real-time database")
+        console.log('👉 Returned data:', response);
+        res.send("Added a new contact to Firebase real-time database : " + response)
+      }, (error) => {
+        console.log(`😱 Axios request failed: ${error}`);
+        res.send(`😱 Axios request failed: ${error}`)
+      })
+})
 
 exports.api = functions.https.onRequest(app)
