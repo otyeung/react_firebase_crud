546aa3aa1fda61bbe6f2306f3dee9db5396bf6d7
Dennis Yeung <otyeung2017@gmail.com>
2020-11-16 09:13:23 +0800

    remove firebase config from index.js

--- functions/index.js
+++ functions/index.js
@@ -1,31 +1,6 @@
-const firebase = require("firebase/app");
 const functions = require('firebase-functions')
+const ref = require('./firebase')
 // const axios = require('axios')
-
-// #### Firebase database initialization
-// Firebase App (the core Firebase SDK) is always required and
-// must be listed before other Firebase SDKs
-
-// Add the Firebase products that you want to use
-//require("firebase/auth");
-require("firebase/database");
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyAKLpVlHdJImwVVU-hM3GQ3BE8ODZ68F2o",
-  authDomain: "react-crud-d487b.firebaseapp.com",
-  databaseURL: "https://react-crud-d487b.firebaseio.com",
-  projectId: "react-crud-d487b",
-  storageBucket: "react-crud-d487b.appspot.com",
-  messagingSenderId: "645779934914",
-  appId: "1:645779934914:web:a3c9454a6f2c67594ab55b"
-};
-
-// Initialize Firebase, returns an object
-//const database = firebase.initializeApp(firebaseConfig)
-firebase.initializeApp(firebaseConfig)
-var database = firebase.database()
-var ref = database.ref('contacts')
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -51,7 +26,7 @@ app.get('/admin', (req, res) => {
 
 // Return all contacts from db
 app.get('/contacts', (req, res) => {
-  const ref = database.ref('contacts')
+  //const ref = firebaseDb.ref('contacts')
 
   // Attach an asynchronous callback to read the data at our posts reference
   ref.on("value", function(snapshot) {
